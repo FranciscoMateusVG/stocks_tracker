66085a706149ff63611bca1d116d1f6b369ce98f
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2023-12-26 19:59:59 -0300

    First

---
+++ src/db/prisma.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
+
+// async function main() {
+//   const newPost = await prisma.post.create({
+//     data: {
+//       title: 'My first post',
+//       content: 'This is the content of the first post',
+//     },
+//   });
+//   console.log(newPost);
+// }
+
+// main()
+//   .catch((e) => {
+//     throw e;
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
